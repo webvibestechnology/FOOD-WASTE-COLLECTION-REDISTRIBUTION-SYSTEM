7c36b16f47c8caaaa61b18b8ac233eda7eb0d01b
swati <paliwalswati603@gmail.com>
2026-09-03 16:21:09 +0530

    backend

---
+++ Backend/food-waste-backend/src/main/java/com/food_waste_backend/Entity/Address.java
@@ -0,0 +1,5 @@
+package com.food_waste_backend.Entity;
+
+public class Address {
+
+}
---
+++ Backend/food-waste-backend/src/test/java/com/food_waste_backend/FoodWasteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_waste_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
